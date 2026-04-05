eb6b12359923e41c0e845f14b36317b605b07044
Abdessalem LTAIEF <abdslambeltayf02@gmail.com>
2026-04-05 12:16:25 +0200

    update

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import '../css/style.css'; // your template CSS
+
+import './main.js';
+// OPTIONAL (if needed)
+import $ from 'jquery';
+window.$ = window.jQuery = $;
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Poseify</title>
+    <title>S.O.B.M.T.I</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Favicon -->
